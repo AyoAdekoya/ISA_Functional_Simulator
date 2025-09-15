075169304ec43ef0680e794beaac0be60dd9061e
Ryan Githae <ryangithae@gmail.com>
2025-09-14 20:20:32 -0400

    Updated sim.h file with opcodes & funct3s and funct7s

--- src/sim.h
+++ src/sim.h
@@ -30,19 +30,63 @@ enum OPCODES {
     // I-type opcodes
     OP_INTIMM  = 0b0010011, // Integer ALU immediate instructions addi, slli, slti, sltiu, xori, srli, srai, ori, andi
     OP_OFFIMM = 0b0000011, // Offset immediate instructions lb, lh, lw, ld, lbu, lhu lwu
-    // ...
+    OP_WORIMM = 0b0011011, // Immediate Word insturctions addiw slliw srliw sraiw
+    OP_LNKREG = 0b1100111, // Jump and Link Register Instruction jalr
+    // R-type opcodes
+    OP_REGFMT = 0b0110011, // Register Format Instructions add, sub, sll, slt, sltu, xor, srl, sra, or, and
+    OP_REGWRD = 0b0111011, // Register Format with word instructions addw, subw, sllw, srlw, sraw
+    // S-type opcodes
+    OP_STRFMT = 0b0100011, //  Store format instructions sb, sh, sw, sd
+    // SB-type opcodes
+    OP_STRBYT = 0b1100011, // Store byte instructions beq, bne, blt, bge, bltu, bgeu
+    // U-type opcodes
+    OP_ADDIMM = 0b0010111, // Add Upper Immediate Instructions auipc
+    OP_LDUIMM = 0b0110111, // Load Upper Immediate Instruction lui
+    //UJ-type opcodes
+    OP_JMPLNK = 0b1101111 // Jump and Link Instruction jal
 };
 
 enum FUNCT3 {
+    // Operating on different sizes
+    FUNCT3_BYT = 0b000, // lb, sb
+    FUNCT3_HLW = 0b001, // lh, sh
+    FUNCT3_WRD = 0b010, // lw, sw
+    FUNCT3_DBL = 0b011, // ld, sd
+    FUNCT3_BYU = 0b100, // lbu
+    FUNCT3_HWU = 0b101, // lhu
+    FUNCT3_WDU = 0b110, // lwu
     // For integer ALU instructions
-    FUNCT3_ADD  = 0b000, // add
-    // ...
+    FUNCT3_ADD  = 0b000, // add addi, addiw, addw 
+    FUNCT3_SUB = 0b000, // sub, subw
+    // For immedieate instructions
+    FUNCT3_SLL = 0b001, // slli slliw, sll, sllwf
+    FUNCT3_SET = 0b010, // slti, slt
+    FUNCT3_STU = 0b011, //sltiu, sltu
+    FUNCT3_XOR = 0b100, // xori, xor
+    FUNCT3_SHIFT = 0b101, // srli, srai, srliw, sraiw, srl, sra, srlw, sraw
+    FUNCT3_OR = 0b110, // ori, or
+    FUNCT3_AND = 0b111, //andi, and
+    // For SB Instructions
+    FUNCT3_BEQ = 0b000, // beq
+    FUNCT3_BNE = 0b001, // bne
+    FUNCT3_BLT = 0b100, // blt
+    FUNCT3_BGE = 0b101, // bge
+    FUNCT3_BLU = 0b110, // blt
+    FUNCT3_BGU = 0b111, // bgeu
 };
 
 enum RI_FUNCT7 {
-    // for R type add/sub instruction
-    FUNCT7_ADD     = 0b0000000, // add
-    // ...
+    // for add/sub instruction
+    FUNCT7_ADD = 0b0000000, // add, addw
+    FUNCT7_SUB = 0b0100000, // sub, subw
+    // for shift instructions
+    FUNCT7_SL = 0b0000000, // sll, srl, srlw, sllw, slli, srli, slliw, srliw
+    FUNCT7_SA = 0b0100000, // sra, srai, sraiw, sraw
+    // for logical operations
+    FUNCT7_AND = 0b0000000, // and
+    FUNCT7_OR = 0b0000000, //or
+    FUNCT7_XOR = 0b0000000, //xor
+
 };
 
 // --------------------------------------------------------------------------
